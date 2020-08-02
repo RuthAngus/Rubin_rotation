d58ca258f1cea10f151b4ebf229ff517951df90d
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2020-08-02 13:53:39 -0400

    adding get mag from mass

--- rubin_rotation/mag_calcs.py
+++ rubin_rotation/mag_calcs.py
@@ -1,4 +1,17 @@
 import numpy as np
+from isochrones.mist.bc import MISTBolometricCorrectionGrid
+bc_grid = MISTBolometricCorrectionGrid(['u', 'g', 'r', 'i', 'z', 'V'])
+from isochrones import get_ichrone
+mist = get_ichrone('mist')
+
+def get_mag_from_mass(mass, age_gyr, feh):
+    log_age = np.log10(age_gyr*1e9)
+    eep = mist.get_eep(mass, age_gyr, feh, accurate=True)
+    teff, logg = mist.interp_value([eep, age_gyr, feh], ['Teff', 'logg'])
+    mags = bc_grid.interp([teff, logg, feh, 0.],
+                          ['u', 'g', 'r', 'i', 'z', 'V'])
+    rmag = mags[2]
+    return teff, rmag
 
 def calc_distance_from_M(M, m=24):
     # m - M = 5log(D) - 5
@@ -9,6 +22,9 @@ def calc_distance_from_M(M, m=24):
 if __name__ == "__main__":
     lim = 15
     faint_lim = 25
+
+    print(get_mag_from_mass(1., 4.56, 0.))
+    assert 0
 
     print(calc_distance_from_M(4.76, m=lim), "-",
           calc_distance_from_M(4.76, m=faint_lim), "kpc, G")  # Sun
